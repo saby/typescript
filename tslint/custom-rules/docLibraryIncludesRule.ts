fc94d8224f0d7f723f6986678e3c161032e716ba
da.zimichev <da.zimichev@tensor.ru>
2019-09-12 17:09:12 +0300

    fix tslint

--- tslint/custom-rules/docLibraryIncludesRule.ts
+++ tslint/custom-rules/docLibraryIncludesRule.ts
@@ -1,9 +1,12 @@
+// @ts-ignore
 import * as Lint from "tslint";
+// @ts-ignore
 import * as ts from "typescript";
 
 export class Rule extends Lint.Rules.AbstractRule {
 
    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+      // @ts-ignore
       return this.applyWithWalker(new DocLibraryIncludesWalker(sourceFile, this.getOptions()));
    }
 }
@@ -26,17 +29,20 @@ class DocLibraryIncludesWalker extends Lint.RuleWalker {
                   }
 
                   if (tag.tagName.escapedText == 'include') {
+                     // @ts-ignore
                      this.addFailure(this.createFailure(tag.getStart(), tag.getWidth(), 'Tag @include is not supported. Do you mean tag @includes?'));
                   }
 
                   if (tag.tagName.escapedText == 'includes') {
                      const parts = tag.comment.split(' ');
                      if (parts.length != 2) {
+                        // @ts-ignore
                         this.addFailure(this.createFailure(tag.getStart(), tag.getWidth(), 'Tag @includes has incorrect format. Usage: @includes Alias ClassName'));
                      }
                      const canonizedTag = parts.join(' ');
 
                      if (isLibary && includes.has(canonizedTag)) {
+                        // @ts-ignore
                         this.addFailure(this.createFailure(tag.getStart(), tag.getWidth(), `Tag @includes '${tag.comment}' is not unique in this library. Check all includes`));
                      }
                      includes.add(canonizedTag);
