1c7c11e06a797902a1aa4e77f19653ba4072f97c
Кудрявцев И.С <is.kudryavcev@corp.tensor.ru>
2021-06-16 08:37:23 +0300

    https://online.sbis.ru/opendoc.html?guid=5087e2ed-1c7c-4635-b6cd-bc9b7c0225d0

--- wasabyGlobalTypings.d.ts
+++ wasabyGlobalTypings.d.ts
@@ -5,16 +5,19 @@ declare module 'i18n!*' {
 }
 
 declare module 'wml!*' {
+    // tslint:disable-next-line: no-any
     const module: any;
     export = module;
 }
 
 declare module 'tmpl!*' {
+    // tslint:disable-next-line: no-any
     const module: any;
     export = module;
 }
 
 declare module 'html!*' {
+    // tslint:disable-next-line: no-any
     const module: any;
     export = module;
 }
@@ -25,16 +28,19 @@ declare module 'css!*' {
 }
 
 declare module 'json!*' {
+    // tslint:disable-next-line: no-any
     const value: any;
     export = value;
 }
 
 declare module 'optional!*' {
+    // tslint:disable-next-line: no-any
     const module: any;
     export = module;
 }
 
 declare module 'browser!*' {
+    // tslint:disable-next-line: no-any
     const value: any;
     export = value;
 }
