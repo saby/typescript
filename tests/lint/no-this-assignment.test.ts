584e6292f7092e3fc65ffb6c24edb4c20678f3e4
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-03-03 18:02:59 +0300

    new change

---
+++ tests/lint/no-this-assignment.test.ts
@@ -0,0 +1,35 @@
+import { ESLint } from 'eslint';
+import { lint } from './utils/lint';
+import { getConfig } from './utils/getConfig';
+
+describe('no-this-assignment', () => {
+    let linter: ESLint;
+
+    beforeAll(() => {
+        linter = new ESLint(getConfig());
+    });
+
+    describe('ошибки', () => {
+        const cases = ['const scope = this;'];
+
+        cases.forEach((text, index) => {
+            it(`индекс теста: ${index}`, async () => {
+                const result = await lint(linter, text);
+
+                expect(result.messages).toMatchSnapshot();
+            });
+        });
+    });
+
+    describe('не ошибки', () => {
+        const cases = ['const { baz } = this;'];
+
+        cases.forEach((text, index) => {
+            it(`индекс теста: ${index}`, async () => {
+                const result = await lint(linter, text);
+
+                expect(result.messages).toMatchSnapshot();
+            });
+        });
+    });
+});
